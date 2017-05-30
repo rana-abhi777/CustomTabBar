55bf61deef4f4646c702d9767a8e2ca7315d40d5
Aseem 14 <aseem14@ELCAP-2.local>
2017-05-30 19:34:34 +0530

    Last commit for day May30

--- CustomTabBar/ViewController.swift
+++ CustomTabBar/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//         self.present(ExampleProvider.customAnimateRemindStyle(implies: false), animated: true, completion: nil)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        self.present(ExampleProvider.customAnimateRemindStyle(implies: false), animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
