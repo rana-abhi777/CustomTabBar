76180012d5b00341ba6a9365e2377125e25f2bfd
Aseem 14 <aseem14@ELCAP-2.local>
2017-05-30 16:57:13 +0530

    first commit

---
+++ CustomTabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTabBar
+//
+//  Created by Aseem 14 on 30/05/17.
+//  Copyright © 2017 Code-Brew Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
